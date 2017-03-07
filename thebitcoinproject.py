395f7ce9329ac7d0ec1b9168ded9fa1f4563b118
Jmisel33 <jmiselnicky@sjnma.org>
2017-03-07 02:03:13 -0600

    created the project

---
+++ thebitcoinproject.py
@@ -0,0 +1,6 @@
+ 
+
+print('Welcome to the bitcoin Calculater. In this application you will be able to learn about the history of bitcoin, find the current value of a bitcoin as well as be able to search from past months and weeks on what bitcoins where priced at.')
+
+print('Enter if you would like to start')
+
